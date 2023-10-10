ec2cce7838ed3ef06f268de9cdcfc5ced2cd8514
manish785 <kumarachilish1997@gmail.com>
2023-10-10 13:02:47 +0530

    feat : added createComment and toggleLike Component

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,7 @@ import Loader from './Loader';
 import Navbar from './Navbar';
 import FriendsList from './FriendsList';
 import CreatePost from './CreatePost';
+import Post from './Post';
+import Comment from './Comment';
 
-export { App, Loader, Navbar, FriendsList, CreatePost };
+export { App, Loader, Navbar, FriendsList, CreatePost, Post, Comment };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,10 @@
-import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-
-import {  Loader, FriendsList, CreatePost } from '../components';
-import { getPosts } from '../api';
+import { Post, Loader, FriendsList, CreatePost } from '../components';
 import styles from '../styles/home.module.css';
 import { useAuth, usePosts } from '../hooks';
 
 const Home = () => {
   const auth = useAuth();
   const posts = usePosts();
-  console.log('posts', posts);
 
   if (posts.loading) {
     return <Loader />;
@@ -20,54 +15,7 @@ const Home = () => {
       <div className={styles.postsList}>
         <CreatePost />
         {posts.data.map((post) => (
-          <div className={styles.postWrapper} key={`post-${post._id}`}>
-            <div className={styles.postHeader}>
-              <div className={styles.postAvatar}>
-                <img
-                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMlJQT1Rc7EKivuy0WFsA1IwFsIIhvricvExrQaUg&s"
-                  alt="user-pic"
-                />
-                <div>
-                  <Link
-                    to={{
-                      pathname: `/user/${post.user._id}`,
-                      state: {
-                        user: post.user,
-                      },
-                    }}
-                    className={styles.postAuthor}
-                  >
-                    {post.user.name}
-                  </Link>
-                  <span className={styles.postTime}>a minute ago</span>
-                </div>
-              </div>
-              <div className={styles.postContent}>{post.content}</div>
-
-              <div className={styles.postActions}>
-                <div className={styles.postLike}>
-                  <img
-                    src="https://png.pngtree.com/png-clipart/20190516/original/pngtree-facebook-like-icon-png-image_3584862.jpg"
-                    alt="likes-icon"
-                  />
-                  <span>{post.likes.length}</span>
-                </div>
-
-                <div className={styles.postCommentsIcon}>
-                  <img
-                    src="https://i.pinimg.com/736x/44/bd/61/44bd61eb2334919917cbf80e025501aa.jpg"
-                    alt="comments-icon"
-                  />
-                  <span>{post.comments.length}</span>
-                </div>
-              </div>
-              <div className={styles.postCommentBox}>
-                <input placeholder="Start typing a comment" />
-              </div>
-
-            
-            </div>
-          </div>
+          <Post post={post} key={`post-${post._id}`} />
         ))}
       </div>
       {auth.user && <FriendsList />}
